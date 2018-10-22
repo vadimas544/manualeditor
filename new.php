46fd397bbe43d0c1afba2a9ba961d32035f70774
Vadim Samoilenko <vadim123544@gmail.com>
2018-10-22 11:07:24 +0300

    Change the size of circles

--- new.php
+++ new.php
@@ -1,16 +1,18 @@
 <html>
+
 <style>
 	 #outer-circle {
-	border: 15px solid #000;
+	margin-left: 200px;
+	border: 15px solid;
    background: #fff;
    border-radius: 50%;
    height: 500px;
    width: 500px;
-   position: relative;
+   //position: relative;
    /* 
     Child elements with absolute positioning will be 
     positioned relative to this div 
-   */
+   
  }
  #inner-circle {
    position: absolute;
@@ -18,9 +20,9 @@
    border-radius: 50%;
    height: 50px;
    width: 50px;
-   /*
+   
     Put top edge and left edge in the center
-   */
+   
    top: 50%;
    left: 50%;
    margin: -25px 0px 0px -25px;
@@ -31,10 +33,26 @@
  }
 
 }
-</style>	
-</html>
-<div id="outer-circle">
-  <div id="inner-circle">
+</style>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+	$( document ).ready(function() {
+		$("#slider").change(function() {
+	 		var slide_amount = $('#slider'). val();
 
-  </div>
+			$("#range").html(slide_amount);
+			$("#outer-circle").css({'border':slide_amount+"px"+" "+"solid"});
+	 	
+		});
+	});
+
+</script>
+<div>
+	<div id="slider_container">
+		<input id="slider" type="range" min="0" max="200" step="5" value="50" />
+	</div>
+ <div id="range">50</div>
 </div>
+<div id="outer-circle">
+</div>
+</html>
